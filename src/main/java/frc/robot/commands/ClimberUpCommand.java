b90b5fc1132c5b41097d6172ee1c0c6c62d535e2
LukasH0808 <harrisonpops7@gmail.com>
2024-02-25 13:04:42 -0500

    Robot stops when the Climber is all the way up

--- src/main/java/frc/robot/commands/ClimberUpCommand.java
+++ src/main/java/frc/robot/commands/ClimberUpCommand.java
@@ -1,11 +1,19 @@
 package frc.robot.commands;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ClimberSubsystem;
 
 public class ClimberUpCommand extends Command{
 
-    ClimberSubsystem _climberSubsystem;
+    ClimberSubsystem _climberSubsystem = new ClimberSubsystem();
+    TalonFX _climberLeftMotor = _climberSubsystem.climberLeftMotor;
+    TalonFX _climberRightMotor = _climberSubsystem.climberRightMotor;
+    private StatusSignal<Double> leftPosition;
+    private StatusSignal<Double> rightPosition;
     double speed;
 
     public ClimberUpCommand(ClimberSubsystem climberSubsystem, double speed){
@@ -18,6 +26,18 @@ public class ClimberUpCommand extends Command{
     public void initialize() {
         _climberSubsystem.climberLeftMotor.set(speed);
         _climberSubsystem.climberRightMotor.set(speed);
+    }
+
+    public void stopBot()
+    {
+        rightPosition = _climberSubsystem.climberLeftMotor.getPosition();
+        leftPosition = _climberSubsystem.climberRightMotor.getPosition();
+        SmartDashboard.putString("Climber Right position", rightPosition.toString());
+        SmartDashboard.putString("Climber Left position", leftPosition.toString());
+        // if(rightPosition.getValue() == 1.0 && leftPosition.getValue() == 1.0) //hardcode later
+        // {
+        //     "Robot" speed = 0;
+        // }
     }
 
     @Override
