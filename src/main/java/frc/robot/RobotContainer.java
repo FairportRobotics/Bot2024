f01a30867b1703bf5d5c51559eecbda899a13f38
The-DOMinator-G <reapergamming13@gmail.com>
2025-11-06 21:16:41 -0500

    Update RobotContainer.java
    Added comment about controller change

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+// Hold A on controller to turn on shooter, and release A to turn it off.
 
 package frc.robot;
 
